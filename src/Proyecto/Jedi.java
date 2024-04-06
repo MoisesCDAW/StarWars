ca9d06a90edfcd650e49adfcd15c1bb604ac3444
MoisesCDAW <moisescamposdaw@gmail.com>
2024-04-06 20:12:52 +0100

    Inicio

---
+++ src/Proyecto/Jedi.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Proyecto;
+
+/**
+ *
+ * @author moise
+ */
+public class Jedi {
+    // Atributos
+    String nombre;
+    int fuerza; // un máximo de 10 puntos
+    ESTADO estado;
+    int midiclorianos; // un máximo de 20,000 mil puntos
+    boolean enamorado;
+    Jedi[] bloqueados;
+    String pareja;
+    String ladoFuerza; // Lado oscuro o el de la luz
+    
+    
+    // Constructor
+    Jedi(){ // PENDIENTE
+        
+    }
+    
+    
+    // Métodos
+    public String presentarse(){ // PENDIENTE
+        return "";
+    }
+    
+    public void enamorarse(String pareja){ // PENDIENTE
+        
+    }
+    
+    public void bloquear(Jedi jedi){ // PENDIENTE
+    
+    }
+    
+    @Override 
+    public String toString(){ // PENDIENTE
+        return "";
+    }
+}
