bfa80c8f5fcb8ab4b4ea3fbc27020b2252dc6b63
MoisesCDAW <moisescamposdaw@gmail.com>
2024-04-06 21:45:02 +0100

    Clase Aprendiz: Se implementó Constructor y los Métodos del enunciado Clase Jedi: Se implementó Constructor y los setters/getters

--- src/Proyecto/Aprendiz.java
+++ src/Proyecto/Aprendiz.java
@@ -8,6 +8,56 @@ package Proyecto;
  *
  * @author moise
  */
-public class Aprendiz {
+public class Aprendiz extends Jedi implements Comparable{
+    // Atributos
+    String maestro;
+    boolean despertarFuerza;
+    
+    // Constructor
+    public Aprendiz(String nombre, int fuerza, int midiclorianos, String ladoFuerza, String Maestro){
+        super(nombre, fuerza, midiclorianos, ladoFuerza);
+        this.maestro = "Sin maestro";
+    }
+    
+    // Métodos
+    public void entrenar(){
+        double aux;
+        aux = this.getMidiclorianos()/200000;
+        if (this.getFuerza()+aux>=10) {
+            System.out.println("Este personaje ha alcanzado su fuerza máxima.");
+            this.setFuerza(10);
+        }else{
+            System.out.println("Entreno hecho!");
+            this.setFuerza((int) (this.getFuerza() + aux));
+        }
+    }
+    
+    public void elegirMaestro(Object o){
+        this.maestro = ((Jedi)o).getNombre();
+    }
+    
+    public void despiertaFuerza(){
+        if (despertarFuerza) {
+            System.out.println("Este personaje ya ha despertado la fuerza.");
+        }else{
+            System.out.println("Fuerza despertada!");
+            this.despertarFuerza=true;
+        }
+    }
+    
+    @Override
+    public String toString(){
+        return super.toString()+String.format("Maestro: %s\nFuerza Despierta? %s\n", this.maestro, this.despertarFuerza);
+    }
+    
+    @Override
+    public boolean equals(Object o){ // PENDIENTE
+        return true;
+    }
+
+    @Override
+    public int compareTo(Object o) { // PENDIENTE
+        return 1;
+    }
     
 }
--- src/Proyecto/Jedi.java
+++ src/Proyecto/Jedi.java
@@ -8,20 +8,32 @@ package Proyecto;
  *
  * @author moise
  */
-public class Jedi {
+public class Jedi implements Comparable{
     // Atributos
-    String nombre;
-    int fuerza; // un máximo de 10 puntos
-    ESTADO estado;
-    int midiclorianos; // un máximo de 20,000 mil puntos
-    boolean enamorado;
-    Jedi[] bloqueados;
-    String pareja;
-    String ladoFuerza; // Lado oscuro o el de la luz
+    private String nombre;
+    private int fuerza; // un máximo de 10 puntos
+    private ESTADO estado;
+    final private int midiclorianos; // un máximo de 20,000 mil puntos
+    private boolean enamorado;
+    private Jedi[] bloqueados;
+    private String pareja;
+    private String ladoFuerza; // Lado oscuro o el de la luz
     
     
     // Constructor
-    Jedi(){ // PENDIENTE
+    Jedi(String nombre, int fuerza, int midiclorianos, String ladoFuerza){
+        setNombre(nombre);
+        setFuerza(fuerza);
+        
+        this.estado = ESTADO.VIVO;
+        
+        if (midiclorianos<1) {
+            System.out.println("Se ingresó una cantidad errónea de midiclorianos.");
+            this.midiclorianos = 1;
+        }else{this.midiclorianos = midiclorianos;}
+        
+        this.pareja = "Sin pareja";
+        setLadoFuerza(ladoFuerza);
         
     }
     
@@ -42,5 +54,75 @@ public class Jedi {
     @Override 
     public String toString(){ // PENDIENTE
         return "";
+    }
+
+    @Override
+    public boolean equals(Object o){ // PENDIENTE
+        return true;
+    }
+
+    @Override
+    public int compareTo(Object o) { // PENDIENTE
+        return 1;
+    }    
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getFuerza() {
+        return fuerza;
+    }
+
+    public void setFuerza(int fuerza) {
+        this.fuerza = fuerza;
+    }
+
+    public ESTADO getEstado() {
+        return estado;
+    }
+
+    public void setEstado(ESTADO estado) {
+        this.estado = estado;
+    }
+
+    public int getMidiclorianos() {
+        return midiclorianos;
+    }
+
+    public boolean isEnamorado() {
+        return enamorado;
+    }
+
+    public void setEnamorado(boolean enamorado) {
+        this.enamorado = enamorado;
+    }
+
+    public Jedi[] getBloqueados() {
+        return bloqueados;
+    }
+
+    public void setBloqueados(Jedi[] bloqueados) {
+        this.bloqueados = bloqueados;
+    }
+
+    public String getPareja() {
+        return pareja;
+    }
+
+    public void setPareja(String pareja) {
+        this.pareja = pareja;
+    }
+
+    public String getLadoFuerza() {
+        return ladoFuerza;
+    }
+
+    public void setLadoFuerza(String ladoFuerza) {
+        this.ladoFuerza = ladoFuerza;
     }
 }
