168ad43aa8c6d30497b2b80392fd7a9374cd1225
MoisesCDAW <moisescamposdaw@gmail.com>
2024-04-10 13:12:33 +0100

    Agregados Se agregó un objeto de la clase "CementerioJedi" y uno de la clase "Jedi" para realizar pruebas de muestra de datos

--- src/Principal/Main.java
+++ src/Principal/Main.java
@@ -4,6 +4,8 @@
  */
 package Principal;
 
+import Proyecto.*;
+
 /**
  *
  * @author moise
@@ -14,7 +16,11 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        CementerioJedi c1 = new CementerioJedi();
+        System.out.println(c1);
+        
+        Jedi j1 = new Jedi("Max", 5, 23);
+        System.out.println(j1);
     }
     
 }
